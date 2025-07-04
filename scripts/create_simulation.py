2aa621b001518a10238bf90dd1b77b551959ec47
bjoverst <bjorn.verstraeten@ugent.be>
2025-07-05 01:45:02 +0200

    Try out fenton-karma model

--- scripts/create_simulation.py
+++ scripts/create_simulation.py
@@ -1,61 +1,83 @@
 import finitewave as fw
+import matplotlib.pyplot as plt
 import numpy as np
 
-if __name__ == "__main__":
-    # set up the tissue:
-    n = 128
+
+def add_cardiac_tissue(model, n):
     tissue = fw.CardiacTissue2D([n, n])
 
-    # non-conductive regions
-    # tissue.mesh[n // 4 - 16 : n // 4 + 16, n // 4 - 16 : n // 4 + 16] = 0
     tissue.mesh[3 * n // 4 - 8 : 3 * n // 4 + 8, n // 4 - 8 : n // 4 + 32] = 0
 
-    tissue.conductivity = np.ones_like(tissue.mesh, dtype=float)
-    slow_zone = np.ones((16, 24)) * np.nan
+    tissue.conductivity = np.ones([n, n], dtype=float)
+    tissue.conductivity[3 * n // 4 - 32 : 3 * n // 4 + 8, n // 4 - 8 : n // 4 + 32] = (
+        0.1
+    )
 
-    gradient = np.linspace(0.1, 1.0, 24)
-    for i, val in enumerate(gradient):
-        slow_zone[:, i] = val
+    plt.imshow(tissue.conductivity * tissue.mesh)
+    plt.show()
 
-    tissue.conductivity[3 * n // 4 - 32 : 3 * n // 4 + 8, n // 4 - 8 : n // 4 + 32] = 0.1
+    model.cardiac_tissue = tissue
 
-    # set up stimulation parameters:
+
+# NOTE: Is this really necessary?
+def prepacing(model):
     stim_sequence = fw.StimSequence()
 
-    s1 = fw.StimVoltageCoord2D(time=0, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
+    for i in [1, 170, 330, 490, 650]:
+        s1 = fw.StimVoltageCoord2D(time=i, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
+        stim_sequence.add_stim(s1)
+
+    model.stim_sequence = stim_sequence
+
+    tracker_sequence = fw.TrackerSequence()
+    action_pot_tracker = fw.ActionPotential2DTracker()
+    action_pot_tracker.step = 1
+    n = model.cardiac_tissue.meta["shape"][0]
+    action_pot_tracker.cell_ind = [n // 2, n // 2]
+    tracker_sequence.add_tracker(action_pot_tracker)
+    model.tracker_sequence = tracker_sequence
+
+    model.t_max = 660
+    model.run()
+
+    time = np.arange(len(action_pot_tracker.output)) * model.dt
+
+    plt.figure()
+    plt.plot(time, action_pot_tracker.output)
+    plt.legend(title="Fenton-Karma")
+    plt.show()
+
+
+def s2_pulse(model):
+    n = model.cardiac_tissue.meta["shape"][0]
     s2 = fw.StimVoltageCoord2D(
-        time=48,
+        time=810,
         volt_value=1,
         x1=3 * n // 4 + 8,
         x2=-1,
-        y1=n // 4,
-        y2=n // 4 + 8,
+        y1=n // 4 - 8,
+        y2=n // 4,
     )
-    stim_sequence.add_stim(s1)
-    stim_sequence.add_stim(s2)
+    model.stim_sequence.add_stim(s2)
 
-    # set up tracker parameters:
-    tracker_sequence = fw.TrackerSequence()
     animation_tracker = fw.Animation2DTracker()
-    animation_tracker.variable_name = "u"  # Specify the variable to track
+    animation_tracker.variable_name = "u"
     animation_tracker.dir_name = "anim_data"
-    animation_tracker.step = 10
-    animation_tracker.overwrite = True  # Remove existing files in dir_name
-    tracker_sequence.add_tracker(animation_tracker)
+    animation_tracker.step = 20
+    animation_tracker.overwrite = True
 
-    # create model object:
-    model = fw.AlievPanfilov2D()
-    # set up numerical parameters:
-    model.eap = 0.002
-    model.dt = 0.01
-    model.dr = 0.3
-    model.t_max = 150
-
-    # add the tissue and the stim parameters to the model object:
-    model.cardiac_tissue = tissue
-    model.stim_sequence = stim_sequence
-    model.tracker_sequence = tracker_sequence
-
-    model.run()
+    model.tracker_sequence.add_tracker(animation_tracker)
+    model.t_max = 1000
+    model.run(initialize=False)
 
     animation_tracker.write(cmap="viridis", fps=60)
+
+
+if __name__ == "__main__":
+    model = fw.FentonKarma2D()
+    model.dt = 0.01
+    model.dr = 0.25
+
+    add_cardiac_tissue(model, n=128)
+    prepacing(model)
+    s2_pulse(model)
