6de15d1940034f39b55d102ac41e9d628a5eb707
bjoverst <bjorn.verstraeten@ugent.be>
2025-07-04 18:02:16 +0200

    Initialize spiral

--- scripts/create_simulation.py
+++ scripts/create_simulation.py
@@ -1,85 +1,50 @@
 import finitewave as fw
-import matplotlib.pyplot as plt
 
-
-class UpdateMesh(fw.Command):
-    """
-    Update the mesh of the cardiac tissue during the simulation.
-    """
-
-    def __init__(self, time, updated_mesh):
-        """
-        Initialize the command with the time and the updated mesh.
-
-        Args:
-            time (int): The time at which the mesh is updated.
-            updated_mesh (numpy.ndarray): The updated mesh.
-        """
-        super().__init__(time)
-        self.updated_mesh = updated_mesh
-
-    def execute(self, model):
-        model.cardiac_tissue.mesh = self.updated_mesh
-        model.compute_weights()
-
-
-def main():
+if __name__ == "__main__":
     # set up the tissue:
     n = 256
     tissue = fw.CardiacTissue2D([n, n])
+
+    # non-conductive regions
     tissue.mesh[n // 4 - 16 : n // 4 + 16, n // 4 - 16 : n // 4 + 16] = 0
-    tissue.mesh[3 * n // 4 - 16 : 3 * n // 4 + 16, n // 4 - 16 : n // 4 + 16] = 0
-
-    tissue.mesh[n // 4 - 32 : n // 4 + 32, 3 * n // 4 - 32 : 3 * n // 4 + 32] = 0
-
-    tissue.mesh[n // 4 - 2 : n // 4 + 2, n // 4 + 16 : 3 * n // 4 - 32] = 2
-
-    mesh_without_block = tissue.mesh.copy()
-    mesh_without_block[n // 4 - 2 : n // 4 + 2, n // 4 + 16 : 3 * n // 4 - 32] = 1
+    tissue.mesh[3 * n // 4 - 16 : 3 * n // 4 + 16, n // 2 - 16 : n // 2 + 16] = 0
 
     # set up stimulation parameters:
     stim_sequence = fw.StimSequence()
-    stim_sequence.add_stim(
-        fw.StimVoltageCoord2D(time=0, volt_value=1, x1=0, x2=5, y1=0, y2=n)
-    )
 
-    command_sequence = fw.CommandSequence()
-    command_sequence.add_command(UpdateMesh(45, mesh_without_block))
+    s1 = fw.StimVoltageCoord2D(time=0, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
+    s2 = fw.StimVoltageCoord2D(
+        time=52,
+        volt_value=1,
+        x1=3 * n // 4 + 16,
+        x2=-1,
+        y1=n // 2,
+        y2=n // 2 + 16,
+    )
+    stim_sequence.add_stim(s1)
+    stim_sequence.add_stim(s2)
+
+    # set up tracker parameters:
+    tracker_sequence = fw.TrackerSequence()
+    animation_tracker = fw.Animation2DTracker()
+    animation_tracker.variable_name = "u"  # Specify the variable to track
+    animation_tracker.dir_name = "anim_data"
+    animation_tracker.step = 10
+    animation_tracker.overwrite = True  # Remove existing files in dir_name
+    tracker_sequence.add_tracker(animation_tracker)
 
     # create model object:
     model = fw.AlievPanfilov2D()
     # set up numerical parameters:
     model.dt = 0.01
     model.dr = 0.3
-    model.t_max = 10
+    model.t_max = 150
 
     # add the tissue and the stim parameters to the model object:
     model.cardiac_tissue = tissue
     model.stim_sequence = stim_sequence
-    model.command_sequence = command_sequence
+    model.tracker_sequence = tracker_sequence
 
     model.run()
-    u_10 = model.u.copy()
 
-    model.t_max = 46
-    model.run(initialize=False)
-    u_46 = model.u.copy()
-
-    model.t_max = 150
-    model.run(initialize=False)
-    u_150 = model.u.copy()
-
-    model.t_max = 250
-    model.run(initialize=False)
-    u_250 = model.u.copy()
-
-    fig, axs = plt.subplots(ncols=4)
-    axs[0].imshow(u_10, cmap="hot")
-    axs[1].imshow(u_46, cmap="hot")
-    axs[2].imshow(u_150, cmap="hot")
-    axs[3].imshow(u_250, cmap="hot")
-    plt.show()
-
-
-if __name__ == "__main__":
-    main()
+    animation_tracker.write(cmap="viridis", fps=60)
