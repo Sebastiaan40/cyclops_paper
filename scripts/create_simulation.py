5295d59297ef3d4e55982f4646e1d3b106db876e
bjoverst <bjorn.verstraeten@ugent.be>
2025-07-05 03:25:18 +0200

    Trying out other model parameters

--- scripts/create_simulation.py
+++ scripts/create_simulation.py
@@ -19,35 +19,6 @@ def add_cardiac_tissue(model, n):
     model.cardiac_tissue = tissue
 
 
-# NOTE: Is this really necessary?
-def prepacing(model):
-    stim_sequence = fw.StimSequence()
-
-    for i in [1, 170, 330, 490, 650]:
-        s1 = fw.StimVoltageCoord2D(time=i, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
-        stim_sequence.add_stim(s1)
-
-    model.stim_sequence = stim_sequence
-
-    tracker_sequence = fw.TrackerSequence()
-    action_pot_tracker = fw.ActionPotential2DTracker()
-    action_pot_tracker.step = 1
-    n = model.cardiac_tissue.meta["shape"][0]
-    action_pot_tracker.cell_ind = [n // 2, n // 2]
-    tracker_sequence.add_tracker(action_pot_tracker)
-    model.tracker_sequence = tracker_sequence
-
-    model.t_max = 660
-    model.run()
-
-    time = np.arange(len(action_pot_tracker.output)) * model.dt
-
-    plt.figure()
-    plt.plot(time, action_pot_tracker.output)
-    plt.legend(title="Fenton-Karma")
-    plt.show()
-
-
 def s2_pulse(model):
     n = model.cardiac_tissue.meta["shape"][0]
     s2 = fw.StimVoltageCoord2D(
@@ -60,24 +31,54 @@ def s2_pulse(model):
     )
     model.stim_sequence.add_stim(s2)
 
-    animation_tracker = fw.Animation2DTracker()
-    animation_tracker.variable_name = "u"
-    animation_tracker.dir_name = "anim_data"
-    animation_tracker.step = 20
-    animation_tracker.overwrite = True
-
-    model.tracker_sequence.add_tracker(animation_tracker)
-    model.t_max = 1000
-    model.run(initialize=False)
-
-    animation_tracker.write(cmap="viridis", fps=60)
-
 
 if __name__ == "__main__":
     model = fw.FentonKarma2D()
     model.dt = 0.01
     model.dr = 0.25
+    model.t_max = 400
 
-    add_cardiac_tissue(model, n=128)
-    prepacing(model)
-    s2_pulse(model)
+    # MBR
+    model.tau_r = 50
+    model.tau_o = 8.3
+    model.tau_d = 0.172  # FIX: not in paper
+    model.tau_si = 45
+    model.tau_v_m = 1000
+    model.tau_v_p = 3.33
+    model.tau_w_m = 11
+    model.tau_w_p = 667
+    model.k = 10  # FIX: not in paper
+    model.u_c = 0.13
+    model.uc_si = 0.85
+
+    n = 128
+    tissue = fw.CardiacTissue2D([n, n])
+    model.cardiac_tissue = tissue
+
+    stim_sequence = fw.StimSequence()
+
+    s1 = fw.StimVoltageCoord2D(time=0, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
+    stim_sequence.add_stim(s1)
+
+    s1 = fw.StimVoltageCoord2D(time=300, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
+    stim_sequence.add_stim(s1)
+
+    # s2 = fw.StimVoltageCoord2D(
+    #     time=400, volt_value=1, x1=0, x2=3 * n // 4, y1=n // 2 - 5, y2=n // 2 + 5
+    # )
+    # stim_sequence.add_stim(s2)
+
+    model.stim_sequence = stim_sequence
+
+    tracker_sequence = fw.TrackerSequence()
+    animation_tracker = fw.Animation2DTracker()
+    animation_tracker.variable_name = "u"
+    animation_tracker.dir_name = "anim_data"
+    animation_tracker.step = 20
+    animation_tracker.overwrite = True
+    tracker_sequence.add_tracker(animation_tracker)
+    model.tracker_sequence = tracker_sequence
+
+    model.run()
+
+    animation_tracker.write(cmap="viridis", fps=60)
