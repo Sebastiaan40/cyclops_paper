353ef686d4442b2c53c82f9016a1dc9285162720
bjoverst <bjorn.verstraeten@ugent.be>
2025-07-07 15:35:50 +0200

    Create a semi-stable spiral with phase defects between holes

--- scripts/create_simulation.py
+++ scripts/create_simulation.py
@@ -22,7 +22,7 @@ def add_cardiac_tissue(model, n):
 def s2_pulse(model):
     n = model.cardiac_tissue.meta["shape"][0]
     s2 = fw.StimVoltageCoord2D(
-        time=810,
+        time=50,
         volt_value=1,
         x1=3 * n // 4 + 8,
         x2=-1,
@@ -33,40 +33,25 @@ def s2_pulse(model):
 
 
 if __name__ == "__main__":
-    model = fw.FentonKarma2D()
+    model = fw.AlievPanfilov2D()
     model.dt = 0.01
     model.dr = 0.25
-    model.t_max = 400
-
-    # MBR
-    model.tau_r = 50
-    model.tau_o = 8.3
-    model.tau_d = 0.172  # FIX: not in paper
-    model.tau_si = 45
-    model.tau_v_m = 1000
-    model.tau_v_p = 3.33
-    model.tau_w_m = 11
-    model.tau_w_p = 667
-    model.k = 10  # FIX: not in paper
-    model.u_c = 0.13
-    model.uc_si = 0.85
+    model.t_max = 500
 
     n = 128
     tissue = fw.CardiacTissue2D([n, n])
+    tissue.mesh[n // 3 - 8 : n // 3 + 8, n // 3 - 8 : n // 3 + 8] = 0
+    tissue.mesh[n // 3 - 8 : n // 3 + 8, n // 3 + 16 : n // 3 + 32] = 0
     model.cardiac_tissue = tissue
 
     stim_sequence = fw.StimSequence()
 
-    s1 = fw.StimVoltageCoord2D(time=0, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
-    stim_sequence.add_stim(s1)
+    for i in [0, 40, 70]:
+        s1 = fw.StimVoltageCoord2D(time=i, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
+        stim_sequence.add_stim(s1)
 
-    s1 = fw.StimVoltageCoord2D(time=300, volt_value=1, x1=0, x2=-1, y1=0, y2=5)
-    stim_sequence.add_stim(s1)
-
-    # s2 = fw.StimVoltageCoord2D(
-    #     time=400, volt_value=1, x1=0, x2=3 * n // 4, y1=n // 2 - 5, y2=n // 2 + 5
-    # )
-    # stim_sequence.add_stim(s2)
+    s2 = fw.StimVoltageCoord2D(time=95, volt_value=1, x1=0, x2=n//3, y1=0, y2=n//3)
+    stim_sequence.add_stim(s2)
 
     model.stim_sequence = stim_sequence
 
@@ -74,11 +59,25 @@ if __name__ == "__main__":
     animation_tracker = fw.Animation2DTracker()
     animation_tracker.variable_name = "u"
     animation_tracker.dir_name = "anim_data"
-    animation_tracker.step = 20
+    animation_tracker.step = 10
     animation_tracker.overwrite = True
     tracker_sequence.add_tracker(animation_tracker)
+
+    action_pot_tracker = fw.ActionPotential2DTracker()
+    action_pot_tracker.cell_ind = [n//3 + 10, n // 3 - 8]
+    tracker_sequence.add_tracker(action_pot_tracker)
+
     model.tracker_sequence = tracker_sequence
 
     model.run()
+
+    time = np.arange(len(action_pot_tracker.output)) * model.dt
+
+    plt.figure()
+    plt.plot(time, action_pot_tracker.output)
+    plt.legend(title="Aliev-Panfilov")
+    plt.xlabel("t[ms]")
+    plt.ylabel("E[mV]")
+    plt.show()
 
     animation_tracker.write(cmap="viridis", fps=60)
