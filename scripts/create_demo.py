621ffd3cdfd9cad8108b20c3d19e1e18bd66befb
bjoverst <bjorn.verstraeten@ugent.be>
2025-07-15 01:04:04 +0200

    Add focal beat

--- scripts/create_demo.py
+++ scripts/create_demo.py
@@ -7,11 +7,8 @@ import numpy as np
 # number of nodes on the side
 n = 400
 tissue = fw.CardiacTissue2D([n, n])
-tissue.mesh[n // 4 - 32 : n // 4 + 32, n // 2 - 32 : n // 2 + 32] = 0
-tissue.conductivity = 0.1 * np.ones([n, n])
-tissue.conductivity[
-    3 * n // 4 - 64 : 3 * n // 4 + 64, 3 * n // 4 - 32 : 3 * n // 4 + 32
-] = 1
+tissue.mesh[n // 4 - 64 : n // 4 + 64, n // 2 - 64 : n // 2 + 64] = 0
+tissue.conductivity = 0.15 * np.ones([n, n])
 
 # show conductivity of mesh
 plt.imshow(tissue.mesh * tissue.conductivity)
@@ -19,7 +16,20 @@ plt.show()
 
 # set up stimulation parameters:
 stim_sequence = fw.StimSequence()
-stim_sequence.add_stim(fw.StimVoltageCoord2D(60, 1, n // 4 + 32, 3 * n // 4, 0, n // 2))
+s2 = fw.StimVoltageCoord2D(60, 1, n // 4, 3 * n // 4, 0, n // 2)
+stim_sequence.add_stim(s2)
+
+interval = 320
+for i in range(1, 11):
+    focal = fw.StimVoltageCoord2D(
+        i * interval,
+        1,
+        3 * n // 4 - 32,
+        3 * n // 4 + 32,
+        3 * n // 4 - 32,
+        3 * n // 4 + 32,
+    )
+    stim_sequence.add_stim(focal)
 
 # set up tracker:
 tracker_sequence = fw.TrackerSequence()
@@ -34,7 +44,7 @@ tracker_sequence.add_tracker(animation_tracker)
 fenton_karma = fw.FentonKarma2D()
 fenton_karma.dt = 0.01
 fenton_karma.dr = 0.25
-fenton_karma.t_max = 400
+fenton_karma.t_max = 3200
 
 # add the tissue and the stim parameters to the model object:
 fenton_karma.cardiac_tissue = tissue
